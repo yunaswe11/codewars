cd9be0d4a17831679ea90d43de4bb13d7fca0015
Yuna <yunaswe11@gmail.com>
2024-12-17 19:05:30 -0500

    Created codewars folder and debugging-multiply js file

---
+++ 8 Kyu/Fundamentals/dubugging-mutiply.js
@@ -0,0 +1,16 @@
+
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+// My answer (O)
+
+function multiply(a, b){
+  return a * b
+}
+
+// Other answers
+
+ multiply = (a, b) => a * b;
